a4c093c4f45bf776b86d8ee2e82d9bc82086fd97
Kantachat Puagkajee <kantachat@Kantachats-Mac.local>
2018-09-08 22:35:30 -0700

    Initial Commit

---
+++ The Listener/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Listener
+//
+//  Created by Kantachat Puagkajee on 8/9/2561 BE.
+//  Copyright © 2561 Kantachat Puagkajee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
