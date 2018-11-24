82ada27f4b3c32806e847a19c99ab80bd161dd2c
Kanta'MacPro <mfireball@kantamacpros-macbook-pro.local>
2018-11-24 15:44:57 +0700

    scanner page details

--- The Listener/ScannerViewController.swift
+++ The Listener/ScannerViewController.swift
@@ -13,12 +13,25 @@ class ScannerViewController: UIViewController,AVAudioPlayerDelegate {
     //MARK: Properties
     var audioPlayer: AVAudioPlayer?
     var audioRecorder: AVAudioRecorder?
+    var customGreen: UIColor?
+    var customRed: UIColor?
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var scanPanel: UIImageView!
+    
+    //MARK: Graphic Properties
+    let backgroundImage = UIImage(named: "ScanWallpaperIMG")
+    let scanPanelImage = UIImage(named: "scan-1")
+    let scanUnRecordImage = UIImage(named: "scan-norecord")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //graphic
+        assignGraphic()
+         customGreen = hexStringToUIColor(hex: "#5BC78F")
+         customRed = hexStringToUIColor(hex: "#FF473A")
+        
+        
         let fileManager = FileManager()
         let documentFolderUrl = try? fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
         let fileURL = documentFolderUrl?.appendingPathComponent("Recording:m4a")
@@ -29,17 +42,57 @@ class ScannerViewController: UIViewController,AVAudioPlayerDelegate {
             audioRecorder = nil
         }
     }
+    //Graphic
+   func assignGraphic(){
+    let background = backgroundImage
+    let imageView = UIImageView(frame: view.bounds)
+    imageView.contentMode = UIViewContentMode.scaleAspectFill
+    imageView.clipsToBounds = true
+    imageView.image = background
+    imageView.center = view.center
+    view.addSubview(imageView)
+    self.view.sendSubview(toBack: imageView)
+    
+    //ScanPanel
+    scanPanel.image = scanUnRecordImage
+    }
+    // Hex Color
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
     //Actions
     
     @IBAction func recordingMethod(_ sender: Any) {
         if audioRecorder!.isRecording {
             audioRecorder!.stop()
             recordButton.setTitle("Record", for: UIControlState.normal)
+            recordButton.backgroundColor = customGreen
             playButton.isEnabled = true
+            scanPanel.image = scanUnRecordImage
         } else {
             audioRecorder!.record()
             recordButton.setTitle("Stop", for: UIControlState.normal)
+            recordButton.backgroundColor = customRed
             playButton.isEnabled = false
+            scanPanel.image = scanPanelImage
         }
     }
     
