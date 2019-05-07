43ba4aaa3b8067a4eee9ba29b7d8e59696c01e0e
kantachat <kantachat_2539@hotmail.com>
2019-05-08 00:14:31 +0700

    The Listener : Complete Project

--- The Listener/SearchViewController.swift
+++ The Listener/SearchViewController.swift
@@ -8,20 +8,41 @@
 
 import UIKit
 
-class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource,UISearchBarDelegate  {
     //MARK: Outlets
     
     @IBOutlet weak var searchTableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
-    var songArray = ["A","B","C","D","E","F","G","H"]
+    var songArray = ["7 rings","Girls Like You","Never Be the Same","Paris in the rain","If I Can't Have You","ME!","Love Story","RED","End Game","I Knew You Were Trouble","Yeowooya","Shiny Boy","Counting Stars at Night","The Way To Me","Creep","House Of The Rising Sun"]
+//var songArray = ["A","B","C","Abstract","Barbie","Cat"]
+    var filterData = [String]()
+    var isSearching = false
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        searchTableView.delegate = self
+        searchBar.delegate = self
         assignBackGround()
         
+        
+        searchBar.returnKeyType = UIReturnKeyType.done
     }
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+    
+    //MARK: Search Functions
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            isSearching = false
+            
+            view.endEditing(true)
+            searchTableView.reloadData()
+        } else {
+            isSearching = true
+            
+            filterData = songArray.filter({$0 == searchBar.text})
+            searchTableView.reloadData()
+        }
     }
     //MARK: Actions
     @IBAction func cancelButton(_ sender: UIButton) {
@@ -30,17 +51,35 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     //MARK: TableviewDelegates
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isSearching {
+            return filterData.count
+        }
         return songArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellIdentifier = "searchcell"
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? searchTableViewCell else {
-            fatalError("The dequeed cell is not an instance of PlayListTableViewCell")
+//        let cellIdentifier = "searchcell"
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? searchTableViewCell else {
+//            fatalError("The dequeed cell is not an instance of PlayListTableViewCell")
+//        }
+        if let cell = searchTableView.dequeueReusableCell(withIdentifier: "searchcell", for: indexPath) as? searchTableViewCell {
+        
+        var text: String!
+        
+        if isSearching {
+            text = filterData[indexPath.row]
+        } else {
+            text = songArray[indexPath.row]
         }
         
-        cell.songNameLabel.text = songArray[indexPath.row]
+            cell.songNameLabel.text = text
+    
+        
+       // cell.songNameLabel.text = songArray[indexPath.row]
         return cell
+        } else {
+            return UITableViewCell()
+        }
     }
     
     func assignBackGround(){
